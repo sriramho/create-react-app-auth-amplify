e628a96d34b557a96789d343db5b359c29b1b6cc
sriramho <46807608+sriramho@users.noreply.github.com>
2021-02-14 21:10:58 +0100

    Updated.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hls from 'hls.js';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+let player = document.querySelector('#player');
+let button = document.querySelector('#button');
+
+const play = function(){
+  console.log('event',button.innerHTML);
+  if(button.innerHTML === 'Play'){
+      player.play();
+      button.innerHTML = "Pause";
+  }else{
+    
+      player.pause();
+      button.innerHTML = "Play";
   }
-}
+};
+console.log(Hls.Events.ERROR);
+if(Hls.isSupported()) {
+    button.innerHTML = 'Loading';
+    var hls = new Hls();
+    hls.loadSource('https://d27k1ozkf91br.cloudfront.net/5c6c476e-46df-4c46-ba35-09010f82aa03/hls/file_example_MOV_1280_1_4MB.m3u8');
+    hls.attachMedia(player);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+      button.innerHTML = 'Start Stream';
+      button.addEventListener('click',play);
+  });
+  hls.on(Hls.Events.ERROR,function(err){
+    console.log(err)
+  })
+ }else{
+   button.innerHTML = 'Not Supported';
+ }
+
+//player.play();
 
 export default withAuthenticator(App, true);
